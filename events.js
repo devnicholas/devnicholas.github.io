eb4f325050b5d00b8a2fbb6f780453621ea738cf
devnicholas <46843036+devnicholas@users.noreply.github.com>
2019-01-19 12:35:08 -0200

    Update events.js

--- events.js
+++ events.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded',function(event){
      if (typeof dataText[i] == 'undefined'){
         setTimeout(function() {
           StartTextAnimation(0);
-        }, 20000);
+        }, 5000);
      }
      // check if dataText[i] exists
     if (i < dataText[i].length) {
